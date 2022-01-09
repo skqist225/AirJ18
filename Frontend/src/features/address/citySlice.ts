db34429babefb0f5cac4a473823ba14641249d0b
skqist225 <74346301+skqist225@users.noreply.github.com>
2022-01-09 13:12:47 +0700

    9/1/2022 1:12Pm

---
+++ Frontend/src/features/address/citySlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk, isAnyOf } from '@reduxjs/toolkit';
+import api from '../../axios';
+
+export const fetchCountries = createAsyncThunk(
+    'country/fetchCountries',
+    async (_, { dispatch, getState, rejectWithValue }) => {
+        try {
+            const { data } = await api.get(`/countries`);
+            return { data };
+        } catch (error) {}
+    }
+);
+
+interface ICountry {
+    id: number;
+    name: string;
+    code: string;
+    dialCode: string;
+}
+
+type CountryState = {
+    countries: ICountry[];
+    loading: boolean;
+};
+
+const initialState: CountryState = {
+    countries: [],
+    loading: true,
+};
+
+const citySlice = createSlice({
+    name: 'country',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchCountries.fulfilled, (state, { payload }) => {
+                state.loading = false;
+                state.countries = payload?.data;
+            })
+            .addMatcher(isAnyOf(fetchCountries.pending), state => {
+                state.loading = true;
+            })
+            .addMatcher(isAnyOf(fetchCountries.rejected), (state, { payload }) => {
+                state.loading = false;
+            });
+    },
+});
+
+export default citySlice.reducer;
