f409b1490de465be63dbe3083f316f7662d0e378
Le Minh Thuan <thuan.leminhthuan.10.2@gmail.com>
2022-06-12 00:03:56 +0700

    12/06/2022 12:03AM

--- Backend/src/main/java/com/airtnt/airtntapp/sms/VerifyOTPController.java
+++ Backend/src/main/java/com/airtnt/airtntapp/sms/VerifyOTPController.java
@@ -1,18 +1,23 @@
 package com.airtnt.airtntapp.sms;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.airtnt.airtntapp.response.StandardJSONResponse;
+import com.airtnt.airtntapp.response.error.BadResponse;
+import com.airtnt.airtntapp.response.success.OkResponse;
+
 @RestController
 public class VerifyOTPController {
 
-    @PostMapping("/otp")
-    public String verifyOTP(@RequestBody TempOTP sms) {
+    @PostMapping("/api/verify-otp")
+    public ResponseEntity<StandardJSONResponse<String>> verifyOTP(@RequestBody TempOTP sms) {
         if (sms.getOtp() == StoreOTP.getOtp()) {
-            return "Correct OTP";
+            return new OkResponse<String>("Correct OTP").response();
         }
 
-        return "Incorrect OTP";
+        return new BadResponse<String>("Incorrect OTP").response();
     }
 }
